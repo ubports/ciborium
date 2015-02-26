2312a637089b0c2c777a7e7b777b10ba8e4df347
Manuel de la Pena <manuel.delapena@canonical.com>
2015-02-26 12:34:55 +0100

    Small typo.

--- cmd/ciborium/main.go
+++ cmd/ciborium/main.go
@@ -236,7 +236,7 @@ func main() {
 // createStandardHomeDirs creates directories reflecting a standard home, these
 // directories are Documents, Downloads, Music, Pictures and Videos
 func createStandardHomeDirs(mountpoint string) error {
-	log.Println("createStandardHomeDirs(", mountPointi, ")")
+	log.Println("createStandardHomeDirs(", mountPoint, ")")
 	for _, node := range []string{"Documents", "Downloads", "Music", "Pictures", "Videos"} {
 		dir := filepath.Join(mountpoint, node)
 
