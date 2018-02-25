77ec423168c57d6056ce472e07658e65543daefb
Marius Gripsgard <marius@ubports.com>
2018-02-25 01:34:38 +0100

    Update bridge.go

--- qml.v1/bridge.go
+++ qml.v1/bridge.go
@@ -1,6 +1,6 @@
 package qml
 
-// #cgo CPPFLAGS: -I./cpp -isystem /usr/include/arm-linux-gnueabihf/qt5/QtCore/5.9.1 -isystem /usr/include/arm-linux-gnueabihf/qt5/QtCore/5.9.1/QtCore
+// #cgo CPPFLAGS: -I./cpp -isystem /usr/include/arm-linux-gnueabihf/qt5/QtCore/5.9.3 -isystem /usr/include/arm-linux-gnueabihf/qt5/QtCore/5.9.3/QtCore -isystem /usr/include/arm-linux-gnueabihf/qt5/QtCore/5.9.3
 // #cgo CXXFLAGS: -std=c++0x -pedantic-errors -Wall -fno-strict-aliasing
 // #cgo LDFLAGS: -lstdc++
 // #cgo pkg-config: Qt5Core Qt5Widgets Qt5Quick
